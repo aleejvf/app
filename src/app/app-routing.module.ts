d4abc782912608f830f47b9a7006d1347757c545
fcomendez <fr.mendez@duocuc.cl>
2024-09-24 17:24:30 -0300

    modificado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -44,11 +44,18 @@ const routes: Routes = [
     path: 'info-uc',
     loadChildren: () => import('./pages/info-uc/info-uc.module').then( m => m.InfoUcPageModule)
   },
+
+  /* {
+    path: 'registro-uc',
+    loadChildren: () => import('./pages/registro-uc/registro-uc.module').then( m => m.RegistroUcPageModule)
+  }, */
+
   
   {
     path: 'desc-viajes-2-us',
     loadChildren: () => import('./pages/desc-viajes-2-us/desc-viajes-2-us.module').then( m => m.DescViajes2UsPageModule)
   },
+
   {
     path: 'verificacion-uc',
     loadChildren: () => import('./pages/verificacion-uc/verificacion-uc.module').then( m => m.VerificacionUcPageModule)
@@ -65,11 +72,13 @@ const routes: Routes = [
     path: 'index-uc',
     loadChildren: () => import('./pages/index-uc/index-uc.module').then( m => m.IndexUcPageModule)
   },
+
   {
     path: 'crear-viaje-uc',
     loadChildren: () => import('./pages/crear-viaje-uc/crear-viaje-uc.module').then( m => m.CrearViajeUcPageModule)
   },
   {
+
     path: 'vehicle-form',
     loadChildren: () => import('./pages/vehicle-form/vehicle-form.module').then( m => m.VehicleFormPageModule)
   },
@@ -84,13 +93,61 @@ const routes: Routes = [
   {
     path: 'confi-vehiculo',
     loadChildren: () => import('./pages/confi-vehiculo/confi-vehiculo.module').then( m => m.ConfiVehiculoPageModule)
-  },  {
+  },
+  {
     path: 'historial-us',
     loadChildren: () => import('./pages/historial-us/historial-us.module').then( m => m.HistorialUsPageModule)
   },
 
 
 
+  /* {
+    path: 'registro',
+    loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'solicitud-us',
+    loadChildren: () => import('./pages/solicitud-us/solicitud-us.module').then( m => m.SolicitudUsPageModule)
+  },
+  {
+    path: 'index-adm',
+    loadChildren: () => import('./pages/index-adm/index-adm.module').then( m => m.IndexAdmPageModule)
+  },
+  {
+    path: 'reg-us-adm',
+    loadChildren: () => import('./pages/reg-us-adm/reg-us-adm.module').then( m => m.RegUsAdmPageModule)
+  },
+  {
+    path: 'reg-uc-adm',
+    loadChildren: () => import('./pages/reg-uc-adm/reg-uc-adm.module').then( m => m.RegUcAdmPageModule)
+  },
+  {
+    path: 'reg-viajes-adm',
+    loadChildren: () => import('./pages/reg-viajes-adm/reg-viajes-adm.module').then( m => m.RegViajesAdmPageModule)
+  },
+  {
+    path: 'det-viajes-adm',
+    loadChildren: () => import('./pages/det-viajes-adm/det-viajes-adm.module').then( m => m.DetViajesAdmPageModule)
+  },
+  {
+    path: 'det-us-adm',
+    loadChildren: () => import('./pages/det-us-adm/det-us-adm.module').then( m => m.DetUsAdmPageModule)
+  },
+  {
+    path: 'det-uc-adm',
+    loadChildren: () => import('./pages/det-uc-adm/det-uc-adm.module').then( m => m.DetUcAdmPageModule)
+  },
+  {
+    path: 'agregar-us-adm',
+    loadChildren: () => import('./pages/agregar-us-adm/agregar-us-adm.module').then( m => m.AgregarUsAdmPageModule)
+  },
+  {
+    path: 'solic-uc-adm',
+    loadChildren: () => import('./pages/solic-uc-adm/solic-uc-adm.module').then( m => m.SolicUcAdmPageModule)
+  },
+ */
+
+/* >>>>>>> 139affa0c180196d790fc45c320202c2b9c09f87 */
 ];
 
 @NgModule({
